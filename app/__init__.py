287634394dc1ef39aa552b1cffa841033b7bd35d
Carlos Segarra <cs525@njit.edu>
2023-07-18 14:28:24 -0400

    Refactor: added JWT imports and auth blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,24 @@
-from flask import Flask
-from flask_bcrypt import Bcrypt
-from flask_jwt_extended import JWTManager
+from flask import Flask, jsonify, request
+from flask_bootstrap import Bootstrap
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token, create_refresh_token,
+    get_jwt_identity, set_access_cookies, set_refresh_cookies, unset_jwt_cookies
+)
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
 app.config.from_object('config.Config')
 
-bcrypt = Bcrypt(app)
 jwt = JWTManager(app)
+bootstrap = Bootstrap(app)
+
+# Import the auth blueprint after JWTManager is initialized
+from app.auth import auth
+
+# Register the blueprint with the Flask application
+app.register_blueprint(auth, url_prefix='/auth')
 
 from app import routes
