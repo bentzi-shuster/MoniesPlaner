39a751219afec67cfeeb0a86ac8454cde20afe3d
Suhney Ahmadi <sma227@njit.edu>
2023-07-18 12:28:23 -0400

    refactor:rerouting and seperating out logic

--- app/auth.py
+++ app/auth.py
@@ -1,20 +1,35 @@
-from flask import Blueprint, render_template, redirect, url_for, flash
+from app import app, jwt
+from flask import render_template, redirect, url_for, jsonify, request
 from app.forms import LoginForm, RegistrationForm
+from app.jwt import generate_token, verify_token
 
-auth = Blueprint('auth', __name__)
+# ...
 
-@auth.route('/register', methods=['GET', 'POST'])
-def register():
+@app.route('/api/register', methods=['POST'])
+def api_register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash('Account created successfully. You can now log in.', 'success')
-        return redirect(url_for('auth.login'))
-    return render_template('register.html', title='Register', form=form)
+        # Handle the registration logic here
+        # Return the token upon successful registration
+        token = generate_token(form.email.data)
+        return jsonify({'token': token}), 201
+    return jsonify({'errors': form.errors}), 400
 
-@auth.route('/login', methods=['GET', 'POST'])
-def login():
+@app.route('/api/login', methods=['POST'])
+def api_login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Logged in successfully!', 'success')
-        return redirect(url_for('main.home'))
-    return render_template('login.html', title='Login', form=form)
+        # Handle the login logic here
+        # Return the token upon successful login
+        token = generate_token(form.email.data)
+        return jsonify({'token': token}), 200
+    return jsonify({'errors': form.errors}), 400
+
+@app.route('/api/user', methods=['GET'])
+@jwt.required()
+def api_user():
+    token = request.headers.get('Authorization').split()[1]
+    email = verify_token(token)
+    if email:
+        return jsonify({'email': email}), 200
+    return jsonify({'message': 'Invalid token'}), 401
