adadc182768bae1e4dbf6408dbee4f3be7a15e70
Suhney Ahmadi <sma227@njit.edu>
2023-07-18 14:11:07 -0400

    Refactor: adding JWT congfigs

--- config.py
+++ config.py
@@ -1,3 +1,11 @@
+import os
+
+import os
+
 class Config:
-    SECRET_KEY = 'your_secret_key'
-"""suhney was here too"""
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    JWT_TOKEN_LOCATION = ['cookies']
+    JWT_ACCESS_COOKIE_PATH = '/api/'
+    JWT_REFRESH_COOKIE_PATH = '/token/refresh'
