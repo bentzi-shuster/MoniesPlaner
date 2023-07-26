c774c4c23986c013d3cb294559b60b011162e71a
Carlos Segarra <cs525@njit.edu>
2023-07-26 12:07:13 -0400

    fixed profile page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,26 +1,60 @@
-import { withPageAuthRequired } from '@auth0/nextjs-auth0'
-import PageLayout from '../components/page-layout'
-import { User } from '../../interfaces'
+"use client";
 
-type ProfileProps = {
-  user: User
-}
+import { useUser } from "@auth0/nextjs-auth0/client";
+import CodeSnippet from "../components/code-snippet";
+import PageLayout from "../components/page-layout";
+import Image from "next/image";
 
-export default function Profile({ user }: ProfileProps) {
+const Profile = () => {
+  const defaultPicture =
+    "https://cdn.auth0.com/blog/hello-auth0/auth0-user.png";
+  const { user } = useUser();
+
+  if (!user) {
+    return null;
+  }
+
   return (
-    <PageLayout user={user}>
-      <h1>Profile</h1>
-
-      <div>
-        <h3>Profile (server rendered)</h3>
-        <image src={user.picture} alt="user picture" />
-        <p>nickname: {user.nickname}</p>
-        <p>name: {user.name}</p>
+    <PageLayout>
+      <div className="content-layout">
+        <h1 id="page-title" className="content__title">
+          Profile Page
+        </h1>
+        <div className="content__body">
+          <p id="page-description">
+            <span>
+              You can use the <strong>ID Token</strong> to get the profile
+              information of an authenticated user.
+            </span>
+            <span>
+              <strong>Only authenticated users can access this page.</strong>
+            </span>
+          </p>
+          <div className="profile-grid">
+            <div className="profile__header">
+              <Image
+                src={user.picture || defaultPicture}
+                alt="Profile"
+                className="profile__avatar"
+                width={80}
+                height={80}
+              />
+              <div className="profile__headline">
+                <h2 className="profile__title">{user.name}</h2>
+                <span className="profile__description">{user.email}</span>
+              </div>
+            </div>
+            <div className="profile__details">
+              <CodeSnippet
+                title="Decoded ID Token"
+                code={JSON.stringify(user, null, 2)}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </PageLayout>
-  )
-}
+  );
+};
 
-// Protected route, checking authentication status before rendering the page.(SSR)
-// It's slower than a static page with client side authentication
-export const getServerSideProps = withPageAuthRequired()
+export default Profile;
