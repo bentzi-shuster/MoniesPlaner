c1f3be65be607f64be60ccca8fa9f64ca16bfc7a
Carlos Segarra <cs525@njit.edu>
2023-07-27 17:38:41 -0400

    Refactor: refine click functionality and display on hover, nextjsimage

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
+import Image from 'next/image';
 
 type CardProps = {
   title: string;
   imageUrl: string;
-  onClick: () => void; // Add onClick prop
+  onClick: () => void;
 };
 
 const CardComponent = ({ title, imageUrl, onClick }: CardProps) => {
@@ -11,21 +12,25 @@ const CardComponent = ({ title, imageUrl, onClick }: CardProps) => {
 
   return (
     <div
-      className="relative w-48 h-48 rounded-lg overflow-hidden shadow-lg bg-gray-200 hover:bg-gray-300 transition-colors duration-300 cursor-pointer"
+      className="relative w-48 h-48 rounded-lg overflow-hidden shadow-md hover:shadow-xl transition-shadow duration-300 ease-in-out cursor-pointer bg-white"
       onMouseEnter={() => setShowTitle(true)}
       onMouseLeave={() => setShowTitle(false)}
-      onClick={onClick} // Trigger onClick event when the card is clicked
+      onClick={onClick}
+      role="button"
+      tabIndex={0}
     >
-      {/* Image */}
-      <img className="w-full h-full object-cover" src={imageUrl} alt={title} />
-
-      {/* Title */}
+      <Image 
+        src={imageUrl} 
+        alt={title} 
+        layout="fill"
+        objectFit="cover" 
+      />
       <div
-        className={`absolute bottom-0 left-0 w-full bg-black bg-opacity-50 text-white text-xl font-bold py-2 px-4 ${
-          showTitle ? 'opacity-100' : 'opacity-0'
-        } transition-opacity duration-300`}
+        className={`absolute inset-x-0 bottom-0 flex justify-center transition-all duration-300 ease-in-out ${showTitle ? 'opacity-100 transform translate-y-0' : 'opacity-0 transform -translate-y-full'}`}
       >
-        {title}
+        <div className="py-1 px-2 m-2 bg-black bg-opacity-50 text-white text-sm font-bold rounded-lg">
+          {title}
+        </div>
       </div>
     </div>
   );
