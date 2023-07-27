8574332668ccfdf34238022ee55c20c238e57a9d
Carlos Segarra <cs525@njit.edu>
2023-07-27 17:09:10 -0400

    Update: fixed selected card display and added click event to card components

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,47 @@ import PageLayout from './components/page-layout';
 import CardComponent from './components/card';
 import PopupCard from './components/popup-card';
 
-const Home = () => {
-  const { user, isLoading } = useUser();
-  const [selectedCard, setSelectedCard] = useState(null);
+type CardInfo = {
+  title: string;
+  imageUrl: string;
+};
 
-  const handleCardClick = (title: string, imageUrl: string) => {
-    setSelectedCard({ title, imageUrl });
+// Placeholder data
+const cards: CardInfo[] = [
+  {
+    title: "Bad Bear",
+    imageUrl: "https://cdn.discordapp.com/attachments/1069455158922190948/1121622537655308438/47920001.JPG"
+  },
+  {
+    title: "The Mandalorian as a 1990s anime aesthetic",
+    imageUrl: "https://cdn.discordapp.com/attachments/1093012473231196190/1095108770276851804/Carrlos_the_Mandalorian_as_a_1990s_anime_aesthetic_ddd2bdba-45bd-4256-b93b-dce46aae4d2d.png"
+  },
+  {
+    title: "Nature",
+    imageUrl: "https://picsum.photos/seed/3/300"
+  },
+  {
+    title: "Strawberries",
+    imageUrl: "https://picsum.photos/seed/4/300"
+  },
+  {
+    title: "Deep Sea",
+    imageUrl: "https://picsum.photos/seed/5/300"
+  },
+];
+
+const Home: React.FC = () => {
+  const { user, isLoading } = useUser();
+  const [selectedCard, setSelectedCard] = useState<CardInfo | null>(null);
+
+  const handleCardClick = (card: CardInfo) => {
+    setSelectedCard(card);
   };
+
+  const handleCloseCard = () => {
+    setSelectedCard(null);
+  };
+
   return (
     <PageLayout user={user} loading={isLoading}>
       <h1 className="text-3xl">Next.js and Auth0 Example</h1>
@@ -38,51 +72,25 @@ const Home = () => {
         <h2 className="text-xl">Community feed</h2>
         <div className="grid grid-cols-12 gap-4">
           <div className="col-span-8 grid grid-cols-2 gap-4">
-            {/* Add onClick prop to each card */}
-            <CardComponent
-              title="Bad Bear"
-              imageUrl="https://cdn.discordapp.com/attachments/1069455158922190948/1121622537655308438/47920001.JPG"
-              onClick={() =>
-                handleCardClick(
-                  'Bad Bear',
-                  'https://cdn.discordapp.com/attachments/1069455158922190948/1121622537655308438/47920001.JPG'
-                )
-              }
-            />
-            <CardComponent
-              title="The Mandalorian as a 1990s anime aesthetic"
-              imageUrl="https://cdn.midjourney.com/ddd2bdba-45bd-4256-b93b-dce46aae4d2d/grid_0.webp"
-              onClick={() =>
-                handleCardClick(
-                  'The Mandalorian as a 1990s anime aesthetic',
-                  'https://cdn.midjourney.com/ddd2bdba-45bd-4256-b93b-dce46aae4d2d/grid_0.webp'
-                )
-              }
-            />
-            <CardComponent title="Nature" imageUrl="https://picsum.photos/seed/3/300"
-              onClick={() =>
-                handleCardClick(
-                  'Nature',
-                  'https://picsum.photos/seed/3/300'
-                )} />
-            <CardComponent title="Strawberries" imageUrl="https://picsum.photos/seed/4/300"
-              onClick={() =>
-                handleCardClick(
-                  'Strawberries',
-                  'https://picsum.photos/seed/4/300'
-                )} />
-            <CardComponent title="Deep Sea" imageUrl="https://picsum.photos/seed/5/300"
-              onClick={() =>
-                handleCardClick(
-                  'Deep Sea',
-                  'https://picsum.photos/seed/5/300'
-                )} />
+            {/* Mapped over cards */}
+            {cards.map((card) => (
+              <CardComponent
+                key={card.title}
+                title={card.title}
+                imageUrl={card.imageUrl}
+                onClick={() => handleCardClick(card)}
+              />
+            ))}
           </div>
           <div className="col-span-4">
-            {/* Display the selected card's title in the PopupCard */}
+            {/* Rendering PopupCard, passing the selected card's data */}
             <div className="flex flex-col justify-between h-full">
               <div className="flex flex-col gap-4">
-                <PopupCard title={selectedCard?.title} imageUrl={selectedCard?.imageUrl} />
+                <PopupCard
+                  title={selectedCard?.title ?? ''}
+                  imageUrl={selectedCard?.imageUrl ?? ''}
+                  onClose={handleCloseCard}
+                />
               </div>
               <div className="flex-1"></div>
             </div>
@@ -92,5 +100,5 @@ const Home = () => {
     </PageLayout>
   );
 };
-// fast/cached SSR page
-export default Home
+
+export default Home;
