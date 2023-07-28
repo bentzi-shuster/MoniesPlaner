b826899a827d60c925ade30f13c51142d69ae9a7
Carlos Segarra <cs525@njit.edu>
2023-07-28 11:19:05 -0400

    Refactor: Home icon

--- app/components/menu.tsx
+++ app/components/menu.tsx
@@ -1,9 +1,9 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import * as NavigationMenu from '@radix-ui/react-navigation-menu';
 import classNames from 'classnames';
 import { CaretDownIcon, HomeIcon, PlusIcon, PersonIcon, ExitIcon, EnterIcon, FileIcon } from '@radix-ui/react-icons';
 import * as Avatar from '@radix-ui/react-avatar';
-import Link from 'next/link'
+import Link from 'next/link';
 
 type ListItemProps = {
   href: string;
@@ -12,9 +12,10 @@ type ListItemProps = {
 };
 
 type NaviProps = {
-  user?: any
-  loading: boolean
-}
+  user?: any;
+  loading: boolean;
+  onOpenChange?: () => void;
+};
 
 const NavigationMenuDemo = ({ user, loading }: NaviProps) => {
   const ListItem: FC<ListItemProps> = ({ href, title, Icon }) => (
@@ -26,11 +27,21 @@ const NavigationMenuDemo = ({ user, loading }: NaviProps) => {
     </li>
   );
 
+  const [isCaretRotated, setCaretRotation] = useState(false);
+  const [isContentOpen, setContentOpen] = useState(false);
+
+  const handleTriggerHover = (isHovering: boolean) => {
+    if (!isContentOpen) {
+      setCaretRotation(isHovering);
+    }
+  };
+
   return (
     <NavigationMenu.Root className="relative z-10 flex w-screen justify-center bg-white p-4">
       <NavigationMenu.List className="flex items-center gap-4 text-lg text-green-11">
         <NavigationMenu.Item>
-          <Link href="/" className="hover:bg-green3 px-4 py-2 rounded-lg no-underline focus:ring-2 focus:ring-green-7 outline-none">
+          <Link href="/" className="flex justify-center items-center hover:bg-green3 px-4 py-2 rounded-lg no-underline focus:ring-2 focus:ring-green-7 outline-none">
+            <HomeIcon className="mr-2" aria-label="Home" />
             Home
           </Link>
         </NavigationMenu.Item>
@@ -53,13 +64,28 @@ const NavigationMenuDemo = ({ user, loading }: NaviProps) => {
 
         {!loading && user && (
           <NavigationMenu.Item>
-            <NavigationMenu.Trigger className="flex items-center gap-2 hover:bg-green3 px-4 py-2 rounded-lg focus:ring-2 focus:ring-green-7 outline-none">
+            <NavigationMenu.Trigger
+              className="flex items-center gap-2 hover:bg-green3 px-4 py-2 rounded-lg focus:ring-2 focus:ring-green-7 outline-none"
+              onMouseEnter={() => handleTriggerHover(true)}
+              onMouseLeave={() => handleTriggerHover(false)}
+            >
               <Avatar.Root className="h-8 w-8 overflow-hidden rounded-full border-2 border-green-300">
                 <Avatar.Image src={user.picture} alt={user.name} />
               </Avatar.Root>
-              <CaretDownIcon className="transition-transform duration-250 ease-in" />
+              <CaretDownIcon
+                className={classNames("transition-transform duration-250 ease-in", {
+                  "transform rotate-180": isCaretRotated,
+                })}
+              />
             </NavigationMenu.Trigger>
-            <NavigationMenu.Content className="absolute bg-white w-48 rounded-md shadow-lg">
+
+            <NavigationMenu.Content
+              className="absolute bg-white w-48 rounded-md shadow-lg"
+              onOpenChange={(open) => {
+                setContentOpen(open);
+                setCaretRotation(open);
+              }}
+            >
               <ul className="text-sm text-green-11 px-4 py-2 leading-6">
                 <ListItem title="My Profile" href="/profile" Icon={PersonIcon} />
                 <ListItem title="My Plans" href="#" Icon={FileIcon} />
