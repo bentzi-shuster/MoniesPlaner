f5993536d7ce8b97fbebf3c36358d311f27e1754
Benjamin Shuster <bentzishuster613@gmail.com>
2023-07-28 12:14:44 -0400

    create plan dialog draft

--- app/components/menu.tsx
+++ app/components/menu.tsx
@@ -1,10 +1,10 @@
 import React, { FC, useState } from 'react';
 import * as NavigationMenu from '@radix-ui/react-navigation-menu';
 import classNames from 'classnames';
-import { CaretDownIcon, HomeIcon, PlusIcon, PersonIcon, ExitIcon, EnterIcon, FileIcon } from '@radix-ui/react-icons';
+import { CaretDownIcon, HomeIcon, PlusIcon, PersonIcon, ExitIcon, EnterIcon, FileIcon, Cross2Icon } from '@radix-ui/react-icons';
 import * as Avatar from '@radix-ui/react-avatar';
 import Link from 'next/link';
-
+import * as Dialog from '@radix-ui/react-dialog';
 type ListItemProps = {
   href: string;
   title: string;
@@ -29,14 +29,21 @@ const NavigationMenuDemo = ({ user, loading }: NaviProps) => {
 
   const [isCaretRotated, setCaretRotation] = useState(false);
   const [isContentOpen, setContentOpen] = useState(false);
+  const [isDialogOpen, setDialogOpen] = useState(false);
 
   const handleTriggerHover = (isHovering: boolean) => {
     if (!isContentOpen) {
       setCaretRotation(isHovering);
     }
   };
+const handleDialogOpen = () => {
+  setDialogOpen(true);
+  console.log('handleDialogOpen');
+};
+  const handleDialogClose = () => setDialogOpen(false);
 
   return (
+
     <NavigationMenu.Root className="relative z-10 flex w-screen justify-center bg-white p-4">
       <NavigationMenu.List className="flex items-center gap-4 text-lg text-green-11">
         <NavigationMenu.Item>
@@ -46,12 +53,61 @@ const NavigationMenuDemo = ({ user, loading }: NaviProps) => {
           </Link>
         </NavigationMenu.Item>
 
-        <NavigationMenu.Item>
-          <Link href="/about" className="flex justify-center items-center hover:bg-green3 px-4 py-2 rounded-lg no-underline focus:ring-2 focus:ring-green-7 outline-none">
-            <PlusIcon className="mr-2" aria-label="Create Plan" />
-            Create Plan
-          </Link>
-        </NavigationMenu.Item>
+        <Dialog.Root>
+            <Dialog.Trigger asChild>
+              <button className="flex justify-center items-center hover:bg-green3 px-4 py-2 rounded-lg no-underline focus:ring-2 focus:ring-green-7 outline-none">
+                <PlusIcon className="mr-2" aria-label="Create Plan" />
+                Create Plan
+              </button>
+            </Dialog.Trigger>
+            <Dialog.Portal>
+              <Dialog.Overlay className="bg-blackA9 data-[state=open]:animate-overlayShow fixed inset-0" />
+              <Dialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
+                <Dialog.Title className="text-mauve12 m-0 text-[17px] font-medium">
+                  Edit profile
+                </Dialog.Title>
+                <Dialog.Description className="text-mauve11 mt-[10px] mb-5 text-[15px] leading-normal">
+                  Make changes to your profile here. Click save when you're done.
+                </Dialog.Description>
+                <fieldset className="mb-[15px] flex items-center gap-5">
+                  <label className="text-violet11 w-[90px] text-right text-[15px]" htmlFor="name">
+                    Name
+                  </label>
+                  <input
+                    className="text-violet11 shadow-violet7 focus:shadow-violet8 inline-flex h-[35px] w-full flex-1 items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none shadow-[0_0_0_1px] outline-none focus:shadow-[0_0_0_2px]"
+                    id="name"
+                    defaultValue="Pedro Duarte"
+                  />
+                </fieldset>
+                <fieldset className="mb-[15px] flex items-center gap-5">
+                  <label className="text-violet11 w-[90px] text-right text-[15px]" htmlFor="username">
+                    Username
+                  </label>
+                  <input
+                    className="text-violet11 shadow-violet7 focus:shadow-violet8 inline-flex h-[35px] w-full flex-1 items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none shadow-[0_0_0_1px] outline-none focus:shadow-[0_0_0_2px]"
+                    id="username"
+                    defaultValue="@peduarte"
+                  />
+                </fieldset>
+                <div className="mt-[25px] flex justify-end">
+                  <Dialog.Close asChild>
+                    <button className="bg-green4 text-green11 hover:bg-green5 focus:shadow-green7 inline-flex h-[35px] items-center justify-center rounded-[4px] px-[15px] font-medium leading-none focus:shadow-[0_0_0_2px] focus:outline-none">
+                      Save changes
+                    </button>
+                  </Dialog.Close>
+                </div>
+                <Dialog.Close asChild>
+                  <button
+                    className="text-violet11 hover:bg-violet4 focus:shadow-violet7 absolute top-[10px] right-[10px] inline-flex h-[25px] w-[25px] appearance-none items-center justify-center rounded-full focus:shadow-[0_0_0_2px] focus:outline-none"
+                    aria-label="Close"
+                  >
+                    <Cross2Icon />
+                  </button>
+                </Dialog.Close>
+              </Dialog.Content>
+
+            </Dialog.Portal>
+          </Dialog.Root>
 
         {!loading && !user && (
           <NavigationMenu.Item>
@@ -96,7 +152,10 @@ const NavigationMenuDemo = ({ user, loading }: NaviProps) => {
         )}
       </NavigationMenu.List>
     </NavigationMenu.Root>
+
   );
 };
+
+
 
 export default NavigationMenuDemo;
