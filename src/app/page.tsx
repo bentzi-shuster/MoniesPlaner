6d4ff35ff3aa34e4db0dca8da494e8e57781ffb4
Benjamin Shuster <bentzishuster613@gmail.com>
2023-07-30 16:09:56 -0400

    Hero Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,16 +5,28 @@ const Home: React.FC = () => {
 
   return (
   <>
-      <h1 className="text-3xl">Next.js and Auth0 Example</h1>
-      
-        <p>
-          To test the login click on <i>Login</i>
-        </p>
-        <p>
-          Once you have logged in you should be able to navigate between
-          protected routes: client rendered, server rendered profile pages,
-          and <i>Logout</i>
-        </p>
+      <h1 className="text-3xl text-center font-bold pt-20">Monnies Planner!</h1>
+      <div className="mx-auto max-w-2xl py-32 sm:py-20 lg:py-13">
+          <div className="text-center">
+            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl  px-10">
+             Life is better with a plan.
+            </h1>
+            <p className="mt-6 text-lg leading-8 text-gray-600 px-6">
+              Sometimes its hard to grasp the concept of money and how much you will have left after you pay your bills. Thats why we created Monnies Planner. A simple and easy to use site that will help you plan your finances and show you what your dream life will really cost you.
+            </p>
+            <div className="mt-10 flex items-center justify-center gap-x-6">
+              <a
+                href="#"
+                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Make a plan
+              </a>
+              <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+                Learn more <span aria-hidden="true">→</span>
+              </a>
+            </div>
+          </div>
+        </div>
       </>
 
   );
--- src/components/Nav/NavMenuDesktop.tsx
+++ src/components/Nav/NavMenuDesktop.tsx
@@ -4,10 +4,13 @@ import * as Tabs from '@radix-ui/react-tabs';
 import Link from 'next/link';
 
 const NavMenuDesktop = ({navs, isLoggedIn}) => {
+  let [linkpath,setLinkPath] = React.useState(window.location.pathname);
+  
   return (
   <Tabs.Root
     className="flex flex-col w-[100] shadow-[0_2px_10px] shadow-blackA4"
     defaultValue="/"
+    
   >
     <Tabs.List className="shrink-0 flex border-b border-mauve6" aria-label="Nav Bar">
       {navs.map((nav,index)=>{
@@ -21,11 +24,15 @@ const NavMenuDesktop = ({navs, isLoggedIn}) => {
       }
         return (
         <Tabs.Trigger
+
+         data-state={linkpath===nav.Link?"active":"inactive"}
           className="bg-white px-5 h-[45px] flex-1 flex items-center justify-center text-[15px] leading-none text-mauve11 select-none  hover:text-violet11 data-[state=active]:text-violet11 data-[state=active]:shadow-[inset_0_-1px_0_0,0_1px_0_0] data-[state=active]:shadow-current data-[state=active]:focus:relative data-[state=active]:focus:shadow-[0_0_0_2px] data-[state=active]:focus:shadow-black outline-none cursor-default"
           value={nav.Link}
           key={index}
         >
-          <Link href={nav.Link} className="w-full h-full flex items-center justify-center" title={nav.Description}>
+          <Link   onClick={(e)=>{
+          setLinkPath(nav.Link);
+        }} href={nav.Link} className="w-full h-full flex items-center justify-center" title={nav.Description}>
           {nav.Name}
             </Link>
         </Tabs.Trigger>
