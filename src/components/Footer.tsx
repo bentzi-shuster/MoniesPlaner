5b8f28379de3dfb663e3ea16119600e282f57f01
Benjamin Shuster <bentzishuster613@gmail.com>
2023-07-31 16:51:18 -0400

    sticky not fixed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-white border-t border-gray-200 py-6 mt-8 fixed bottom-0 z-10 left-0 right-0">
+    <footer className="bg-white border-t border-gray-200 py-6 mt-8 sticky bottom-0 z-10 left-0 right-0">
       <div className="container mx-auto max-w-screen-lg flex justify-between items-center px-4 ">
         <div className="text-gray-600 text-sm">
           © 2023 <a href="#" className="text-green-600 hover:underline">MoniesPlanner™</a>. All Rights Reserved.
