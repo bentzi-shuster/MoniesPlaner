7e72e3e2a5ad51a4d3121d12f16f579ed0ed3097
Benjamin Shuster <bentzishuster613@gmail.com>
2023-07-31 15:36:26 -0400

    view plan page

---
+++ src/app/plans/[planid]/page.tsx
@@ -0,0 +1,81 @@
+import PlanView from "@/src/components/PlanView/PlanView"
+
+
+export default async function Account({params}:{params:{planid:string}}) {
+    // const session = await getSession();
+    let plans = [
+        {Name:"Plan A",Description:"Rob a bank",Cost:0,Income:0,Img:"https://picsum.photos/900/600"},
+        {Name:"Plan B",Description:"Go To Jail",Cost:" my freedom",Income:0.01,Img:"https://picsum.photos/900/600"},
+        {Name:"Plan C",Description:"Escape Jail",Cost:"However much the guards are willing to take",Income:0.01,Img:"https://picsum.photos/900/600"},
+        {Name:"Plan D",Description:"Dig a hole to China with a spoon",Cost:"A spoon",Income:"Still 0.01",Img:"https://picsum.photos/900/600"},
+        {Name:"Plan 1",Description:"This is a plan",Cost:1000,Income:2000,Img:"https://picsum.photos/900/600"},
+        {Name:"Plan 2",Description:"This is a plan on DRUGS",Cost:1000,Income:2000,Img:"https://picsum.photos/900/600"},
+        {Name:"Plan 3",Description:"This is a plan with a lot of money",Cost:1000,Income:2000,Img:"https://picsum.photos/900/600"},
+        {Name:"Plan 4",Description:"This plan was made by a 5 year old",Cost:1000,Income:2000,Img:"https://picsum.photos/900/600"},
+        {Name:"Plan A",Description:"Rob a bank",Cost:0,Income:0,Img:"https://picsum.photos/900/600"},
+        {Name:"Plan B",Description:"Go To Jail",Cost:" my freedom",Income:0.01,Img:"https://picsum.photos/900/600"},
+        {Name:"Plan C",Description:"Escape Jail",Cost:"However much the guards are willing to take",Income:0.01,Img:"https://picsum.photos/900/600"},
+        {Name:"Plan D",Description:"Dig a hole to China with a spoon",Cost:"A spoon",Income:"Still 0.01",Img:"https://picsum.photos/900/600"},
+        {Name:"Plan 1",Description:"This is a plan",Cost:1000,Income:2000,Img:"https://picsum.photos/900/600"},
+        {Name:"Plan 2",Description:"This is a plan on DRUGS",Cost:1000,Income:2000,Img:"https://picsum.photos/900/600"},
+        {Name:"Plan 3",Description:"This is a plan with a lot of money",Cost:1000,Income:2000,Img:"https://picsum.photos/900/600"},
+        {Name:"Plan 4",Description:"This plan was made by a 5 year old",Cost:1000,Income:2000,Img:"https://picsum.photos/900/600"},
+        {Name:"Plan A",Description:"Rob a bank",Cost:0,Income:0,Img:"https://picsum.photos/900/600"},
+        {Name:"Plan B",Description:"Go To Jail",Cost:" my freedom",Income:0.01,Img:"https://picsum.photos/900/600"},
+        {Name:"Plan C",Description:"Escape Jail",Cost:"However much the guards are willing to take",Income:0.01,Img:"https://picsum.photos/900/600"},
+        {Name:"Plan D",Description:"Dig a hole to China with a spoon",Cost:"A spoon",Income:"Still 0.01",Img:"https://picsum.photos/900/600"},
+        {Name:"Plan 1",Description:"This is a plan",Cost:1000,Income:2000,Img:"https://picsum.photos/900/600"},
+        {Name:"Plan 2",Description:"This is a plan on DRUGS",Cost:1000,Income:2000,Img:"https://picsum.photos/900/600"},
+        {Name:"Plan 3",Description:"This is a plan with a lot of money",Cost:1000,Income:2000,Img:"https://picsum.photos/900/600"},
+        {Name:"Plan 4",Description:"This plan was made by a 5 year old",Cost:1000,Income:2000,Img:"https://picsum.photos/900/600"},
+        {Name:"Plan A",Description:"Rob a bank",Cost:0,Income:0,Img:"https://picsum.photos/900/600"},
+        {Name:"Plan B",Description:"Go To Jail",Cost:" my freedom",Income:0.01,Img:"https://picsum.photos/900/600"},
+        {Name:"Plan C",Description:"Escape Jail",Cost:"However much the guards are willing to take",Income:0.01,Img:"https://picsum.photos/900/600"},
+        {Name:"Plan D",Description:"Dig a hole to China with a spoon",Cost:"A spoon",Income:"Still 0.01",Img:"https://picsum.photos/900/600"},
+        {Name:"Plan 1",Description:"This is a plan",Cost:1000,Income:2000,Img:"https://picsum.photos/900/600"},
+        {Name:"Plan 2",Description:"This is a plan on DRUGS",Cost:1000,Income:2000,Img:"https://picsum.photos/900/600"},
+        {Name:"Plan 3",Description:"This is a plan with a lot of money",Cost:1000,Income:2000,Img:"https://picsum.photos/900/600"},
+        {Name:"Plan 4",Description:"This plan was made by a 5 year old",Cost:1000,Income:2000,Img:"https://picsum.photos/900/600"},
+      ]
+      //if planid is not a number, return 404
+        if(isNaN(parseInt(params.planid))){
+            return (
+                <div>
+                    <h1>404</h1>
+                    </div>
+                )
+        }else{
+            let planid = parseInt(params.planid)
+        return (
+            <div>
+                {/* banner */}
+             <span style={{
+                background: `url(https://picsum.photos/900/600)`,
+                backgroundSize: "cover",
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "center center",
+                }}
+                 className="h-72 w-full text-center text-5xl font-bold flex items-center justify-center">
+                    <div className="bg-white bg-opacity-60 px-4 py-2 border-b-green-800 border-b-4">
+                        {plans[planid].Name}
+                    </div>               
+             </span>
+                {/* description */}
+                <div className="flex flex-col items-center justify-center mt-10">
+                    <div className="w-1/2 text-center text-2xl font-bold">
+                        {plans[planid].Description}
+
+                    </div>
+                    <div className="w-1/2 text-center text-md flex flex-row items-center justify-center gap-4 mt-12">
+                        <span className="bg-green10 text-white px-4 py-2 rounded-full">
+                            Cost: ${plans[planid].Cost}
+                        </span>
+                        <span className="bg-green10 text-white px-4 py-2 rounded-full">
+                            Income: ${plans[planid].Income}
+                        </span>
+                        </div>
+                    </div>
+                </div>
+            )
+        }
+    }
