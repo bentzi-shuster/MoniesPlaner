c2fcbc453d6d23db3d12c3b26ae8c2949af710b4
Benjamin Shuster <bentzishuster613@gmail.com>
2023-07-31 15:50:20 -0400

    open in new tab for plan view

--- src/components/PlanView/Plan.tsx
+++ src/components/PlanView/Plan.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image"
-
+import Link from "next/link"
+import { OpenInNewWindowIcon } from '@radix-ui/react-icons'
 export default function Plan({plan,onClick}) {
 
 return(
     <div onClick={onClick}
-     className="sm:max-w-sm sm:min-w-sm md:max-w-full md:min-w-full lg:max-w-full lg:min-w-full overflow-hidden shadow-[0_0px_4px_1px] shadow-blackA4 bg-white border border-gray-200 cursor-pointer"
+     className="sm:max-w-sm sm:min-w-sm md:max-w-full md:min-w-full lg:max-w-full lg:min-w-full overflow-hidden shadow-[0_0px_4px_1px] shadow-blackA4 bg-white border border-gray-200 cursor-pointer relative"
       style={{
         background: `url(${plan.Img})`,
         backgroundSize: "cover",
@@ -14,7 +15,10 @@ return(
       >
         <div className="bg-white bg-opacity-60 hover:bg-opacity-0 transition duration-250 ease-in-out">
 
-
+<Link href={`/plans/`+plan.id} className="absolute top-0 right-0 p-2"
+ target="_blank" rel="noopener noreferrer">
+  <OpenInNewWindowIcon />
+</Link>
     <div className="px-6 py-4 text-center ">
       <div className="font-extrabold text-xl mb-2 text-black max-w-fit mx-auto rounded-sm px-1 border-b-2 border-b-green10 text-center 
       ">{plan.Name}</div>
