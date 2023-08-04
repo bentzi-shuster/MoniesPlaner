3d10b04abbd634096feafe647c915ef5243da838
Suhney23 <sma227@njit.edu>
2023-08-03 21:15:09 -0400

    Refactor: createPlan form action

--- src/app/_action.ts
+++ src/app/_action.ts
@@ -1,13 +1,8 @@
 "use server"
-import { handleSignup } from "../lib/users";
 import { revalidatePath } from "next/cache";
+import { createPlan } from "../lib/plans";
 
-export async function createBookAction(title: string, author: string) {
- await createBook(title, author);
- revalidatePath("/")
-}
-
-export async function createUserAction(email: string) {
-  await handleSignup(email);
-  revalidatePath("/")
+export async function createPlanAction(name: string){
+  await createPlan(name)
+  revalidatePath('/')
 }
