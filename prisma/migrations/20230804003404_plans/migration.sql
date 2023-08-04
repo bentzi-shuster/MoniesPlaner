1080c642a55037a520add44b6d0ec12e0b51f18f
Suhney23 <73618656+Suhney23@users.noreply.github.com>
2023-08-04 16:57:39 -0400

    Refactor: changed models, integrated oracle

--- prisma/migrations/20230804003404_plans/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - The `visibility` column on the `Plan` table would be dropped and recreated. This will lead to data loss if there is data in the column.
-
-*/
--- AlterTable
-ALTER TABLE "Plan" DROP COLUMN "visibility",
-ADD COLUMN     "visibility" BOOLEAN NOT NULL DEFAULT false;
