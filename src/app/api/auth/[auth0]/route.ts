8d31a36eb83dec539fb541a19889c18f355f2287
Benjamin Shuster <bentzishuster613@gmail.com>
2023-08-04 20:41:39 -0400

    will it build now?

--- src/app/api/auth/[auth0]/route.ts
+++ src/app/api/auth/[auth0]/route.ts
@@ -1,17 +1,17 @@
 import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
-let domain = process.env.AUTH0_BASE_URL;
+// let domain = process.env.AUTH0_BASE_URL;
 export const GET = handleAuth({
     login: handleLogin({
       authorizationParams: {
         prompt: "login",
       },
-      returnTo: domain?domain:"/",
+      returnTo: "/",
     }),
     signup: handleLogin({
       authorizationParams: {
         prompt: "login",
         screen_hint: "signup",
       },
-      returnTo: domain?domain:"/",
+      returnTo: "/",
     }),
   });
