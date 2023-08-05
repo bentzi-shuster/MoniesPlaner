9f92b5fbbf61ee16969efbf200922fc0a2250561
Benjamin Shuster <bentzishuster613@gmail.com>
2023-08-04 20:18:20 -0400

    fix-prod-auth

--- src/app/api/auth/[auth0]/route.ts
+++ src/app/api/auth/[auth0]/route.ts
@@ -1,17 +1,19 @@
 import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
-
+import dotenv from 'dotenv';
+dotenv.config();
+let domain = process.env.AUTH0_BASE_URL;
 export const GET = handleAuth({
     login: handleLogin({
       authorizationParams: {
         prompt: "login",
       },
-      returnTo: "/",
+      returnTo: domain?domain:"/",
     }),
     signup: handleLogin({
       authorizationParams: {
         prompt: "login",
         screen_hint: "signup",
       },
-      returnTo: "/",
+      returnTo: domain?domain:"/",
     }),
   });
