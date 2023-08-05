6610c77d08cd681db6a06963c67c8137608465ed
Benjamin Shuster <bentzishuster613@gmail.com>
2023-08-04 20:40:34 -0400

    see if it will build?

--- src/app/api/auth/[auth0]/route.ts
+++ src/app/api/auth/[auth0]/route.ts
@@ -1,6 +1,4 @@
 import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
-import dotenv from 'dotenv';
-dotenv.config();
 let domain = process.env.AUTH0_BASE_URL;
 export const GET = handleAuth({
     login: handleLogin({
