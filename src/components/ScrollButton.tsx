09e0a8f65e74712be6bf936a197dd28161c07d5d
Carlos <cs525@njit.edu>
2023-08-06 17:50:51 -0400

    Refactor: takes contenttextinput

--- src/components/ScrollButton.tsx
+++ src/components/ScrollButton.tsx
@@ -2,22 +2,22 @@
 import React from "react";
 import { ArrowDownIcon } from "@radix-ui/react-icons";
 
-const ScrollButton = ({ targetId }: { targetId: string }) => {
-    const handleClick = () => {
-      const targetElement = document.getElementById(targetId);
-      if (targetElement) {
-        targetElement.scrollIntoView({ behavior: "smooth" });
-      }
-    };
-    
-    return (
-      <button onClick={handleClick} className="btn group relative w-full flex justify-center py-2 px-4 text-sm font-medium text-[#FA8334] hover:underline">
-        <span className="mr-2">
-          <ArrowDownIcon />
-        </span>
-        <span>See Public Plans</span>
-      </button>
-    );
+const ScrollButton = ({ targetId, contentText }: { targetId: string, contentText: string }) => {
+  const handleClick = () => {
+    const targetElement = document.getElementById(targetId);
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: "smooth" });
+    }
   };
-  
-  export default ScrollButton;
+
+  return (
+    <button onClick={handleClick} className="btn text-[#FA8334] hover:underline">
+      <span className="inline mt-4 flex items-center justify-center">
+        {contentText}
+        <ArrowDownIcon />
+      </span>
+    </button>
+  );
+};
+
+export default ScrollButton;
