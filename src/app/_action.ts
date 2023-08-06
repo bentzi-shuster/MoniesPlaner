ac498e8500f5672e8c2f40f4b373405958161871
Benjamin Shuster <bentzishuster613@gmail.com>
2023-08-06 11:50:35 -0400

    remove _action

--- src/app/_action.ts
+++
@@ -1,8 +0,0 @@
-"use server"
-import { revalidatePath } from "next/cache";
-import { createPlan } from "../lib/plans";
-
-export async function createPlanAction(name: string){
-  await createPlan(name)
-  revalidatePath('/')
-}
