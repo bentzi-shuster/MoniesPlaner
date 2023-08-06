ff4f420dd85fecee5e4f4c75df64f1640795de4c
Benjamin Shuster <bentzishuster613@gmail.com>
2023-08-06 15:07:56 -0400

    and envs

--- src/app/api/auth/[auth0]/route.js
+++ src/app/api/auth/[auth0]/route.js
@@ -1,18 +1,18 @@
 import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
 
-export const GET = handleAuth({
-    login: handleLogin({
-      authorizationParams: {
-        prompt: "login",
-      },
-      returnTo: "/",
-    }),
-    signup: handleLogin({
-      authorizationParams: {
-        prompt: "login",
-        screen_hint: "signup",
-      },
-      returnTo: "/",
-    }),
-    baseURL: "http://localhost:3000",
-  });
+// export const GET = handleAuth({
+//     login: handleLogin({
+//       authorizationParams: {
+//         prompt: "login",
+//       },
+//       returnTo: "/",
+//     }),
+//     signup: handleLogin({
+//       authorizationParams: {
+//         prompt: "login",
+//         screen_hint: "signup",
+//       },
+//       returnTo: "/",
+//     }),
+//   });
+export const GET = handleAuth({baseUrl: process.env.AUTH0_BASE_URL});
