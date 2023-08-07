747ee0cc352549ceefddb372c3db3248b29bcf7c
Suhney23 <73618656+Suhney23@users.noreply.github.com>
2023-08-06 23:23:08 -0400

    Refactor: added responsive design

--- src/components/CookiesDialog.tsx
+++ src/components/CookiesDialog.tsx
@@ -20,25 +20,28 @@ const CookieModal: React.FC = () => {
   return (
     <AlertDialog.Root open={isOpen}>
       <AlertDialog.Portal container={typeof window !== 'undefined' ? document.body : undefined}>
-        <AlertDialog.Overlay className="fixed inset-0 bg-black bg-opacity-50" />
-        <AlertDialog.Content className="max-w-md mx-auto p-6 fixed top-[50%] left-[50%] bg-white rounded-lg shadow-lg transform translate-x-[-50%] translate-y-[-50%]">
+        <AlertDialog.Overlay className="bg-blackA9 data-[state=open]:animate-overlayShow fixed inset-0" />
+        <AlertDialog.Content className="max-w-md pr-4 pl-4 pt-8 pb-14 fixed top-[50%] left-[50%] bg-white rounded-lg shadow-lg transform translate-x-[-50%] translate-y-[-50%]">
           <AlertDialog.Title className="text-xl font-semibold mb-4">
             Cookie Consent
           </AlertDialog.Title>
-          <AlertDialog.Description>
-            <p>
-              Our website stores cookies on your device and discloses information in accordance with our Cookie Statement.
-            </p>
-            <p className="mt-2">
-              In accordance with the EU General Data Protection Regulation (GDPR), we need your consent to store and use cookies and other tracking technologies. These technologies help us enhance your browsing experience, display personalized content and ads, analyze website traffic, and better understand our visitors.
-            </p>
+          <AlertDialog.Description className="group max-h-52 overflow-y-hidden hover:overflow-y-auto scrollbar-width-thin">
+              <p>
+                  Our website stores cookies on your device and discloses information in accordance with our Cookie Statement.
+              </p>
+              <p className="mt-2">
+                  In accordance with the EU General Data Protection Regulation (GDPR), we need your consent to store and use cookies and other tracking technologies. These technologies help us enhance your browsing experience, display personalized content and ads, analyze website traffic, and better understand our visitors.
+              </p>
           </AlertDialog.Description>
-          <button
-            className="border border-transparent py-2 px-3 mt-1 font-medium rounded-md text-white bg-[#17B890] hover:bg-[#5EEAD4] "
-            onClick={onClose}
-          >
-            I understand
-          </button>
+
+          <div className="flex justify-center">
+            <button
+              className="border border-transparent py-2 px-3 mt-7 items-center font-medium rounded-md text-white bg-[#17B890] hover:bg-[#5EEAD4] "
+              onClick={onClose}
+            >
+              I understand
+            </button>
+          </div>
         </AlertDialog.Content>
       </AlertDialog.Portal>
     </AlertDialog.Root>
