e085d08fbaf48e8d6410c7bf55a130509ff42111
Kevin Chavez <kjc47@njit.edu>
2023-08-10 16:03:10 -0400

    "Test Facebooklink"

--- tests/components/footer.spec.tsx
+++ tests/components/footer.spec.tsx
@@ -9,3 +9,16 @@ test('Elm renders', async ({ mount }) => {
   const component = await mount(footerElm);
   await expect(component).toBeVisible();
 });
+
+// social media 
+
+test('fb link', async ({ mount ,page}) => {
+  const component = await mount(footerElm);
+  let fblnik = await component.locator('[aria-label="Share on FaceBook!"]').first();
+  await fblnik.click();
+  await page.waitForTimeout(1000);
+  await expect(page.url()).toContain("https://www.facebook.com/login.php");
+  });
+  
+
+
