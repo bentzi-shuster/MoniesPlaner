42cd49acb5b7a07e327a0089de8dc8fc414dea68
Benjamin Shuster <bentzishuster613@gmail.com>
2023-08-10 17:29:53 -0400

    update type in GET

--- src/app/api/webScraper/route.ts
+++ src/app/api/webScraper/route.ts
@@ -1,9 +1,9 @@
-import {NextResponse} from 'next/server';
+import {NextRequest, NextResponse} from 'next/server';
 
 
-export async function GET(page: {nextUrl: URL}){
+export async function GET(request: NextRequest ){
     const webScraperURL = process.env.WEBSCRAPER_URL;
-    let urlToScrape = page.nextUrl.searchParams.get("page")
+    let urlToScrape = request.nextUrl.searchParams.get("page")
     const res = await fetch( webScraperURL + "?page="+ urlToScrape);
     const data = await res.json();
     console.log(data);
