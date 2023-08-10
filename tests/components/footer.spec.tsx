165b0b8189bab3321f07a00562967b8398fd504f
Benjamin Shuster <bentzishuster613@gmail.com>
2023-08-10 19:05:49 -0400

    ✅

--- tests/components/footer.spec.tsx
+++ tests/components/footer.spec.tsx
@@ -21,4 +21,30 @@ test('fb link', async ({ mount ,page}) => {
   });
   
 
+test('twitter link', async ({ mount ,page}) => {
+  const component = await mount(footerElm);
+  let twitterlink = await component.locator('[aria-label="Share on Twitter!"]').first();
+  await twitterlink.click();
+  await page.waitForTimeout(1000);
+  await expect(page.url()).toContain("https://twitter.com/i/flow/login");
+  });
+
+test('linkedin link', async ({ mount ,page}) => {
+  const component = await mount(footerElm);
+  let linkedinlink = await component.locator('[aria-label="Share on LinkedIn!"]').first();
+  await linkedinlink.click();
+  await page.waitForTimeout(1000);
+  await expect(page.url()).toContain("https://www.linkedin.com/signup/cold-join");
+  });
+
+
+test('reddit link', async ({ mount ,page}) => {
+  const component = await mount(footerElm);
+  let redditlink = await component.locator('[aria-label="Share on Reddit!"]').first();
+  await redditlink.click();
+  await page.waitForTimeout(1000);
+  await expect(page.url()).toContain("https://www.reddit.com/login/");
+});
+
+
 
