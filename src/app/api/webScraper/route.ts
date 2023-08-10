ebea03df928691f1084868f95e321f889d5d9321
Benjamin Shuster <bentzishuster613@gmail.com>
2023-08-10 17:25:42 -0400

    patch the webScraper roure

--- src/app/api/webScraper/route.ts
+++ src/app/api/webScraper/route.ts
@@ -1,10 +1,10 @@
 import {NextResponse} from 'next/server';
 
 
-export async function GET(page: string){
+export async function GET(page: {nextUrl: URL}){
     const webScraperURL = process.env.WEBSCRAPER_URL;
-    console.log(webScraperURL);
-    const res = await fetch( webScraperURL + "?page="+ page);
+    let urlToScrape = page.nextUrl.searchParams.get("page")
+    const res = await fetch( webScraperURL + "?page="+ urlToScrape);
     const data = await res.json();
     console.log(data);
     return NextResponse.json({ data });
