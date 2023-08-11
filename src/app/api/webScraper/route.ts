41af56754223102d89c681cead3364d8931b5997
Benjamin Shuster <bentzishuster613@gmail.com>
2023-08-11 11:54:42 -0400

    promise syntax

--- src/app/api/webScraper/route.ts
+++ src/app/api/webScraper/route.ts
@@ -6,8 +6,14 @@ import { NextResponse, NextRequest } from 'next/server';
 export async function GET(request: NextRequest) {
     const webScraperURL = process.env.WEBSCRAPER_URL;
     let urlToScrape = request.nextUrl.searchParams.get("page")
-    const res = await fetch(webScraperURL + "?page=" + urlToScrape)
-    const data = await res.text();
+    const data = await fetch(webScraperURL + "?page=" + urlToScrape)
+    .then((res) => {
+        console.log(res)
+        return res.text();
+    })
+    .catch((e) => {
+        console.log(e)
+    });
     console.log(data);
     return NextResponse.json({ data: data }, { status: 200 });
 }
